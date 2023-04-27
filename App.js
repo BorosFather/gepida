881f2495d9b57e94130009d0663e9c85a93f8de8
RP <mc@kek.hu>
2023-04-27 10:41:48 +0200

    getBikes megírva

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FlatList } from 'react-native-web';
 
 export default function App() {
+
+  cont [bikes, setBikes] = userState([]);
+
+  function getBikes(){
+    let host = "http://localhost:3000/";
+    let endpoit = "bikes";
+    let url = host + endpoit;
+
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      setBikes(result)
+    });
+  }
+  useEffect(() => {
+    getBikes();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Bikes</Text>
+
+      <FlatList data = {bikes} renderItem = { ({bike}) => (<Text>{bike.name}</Text>)} />
       <StatusBar style="auto" />
     </View>
   );
